1a2fc82eb269524b81e8f0bd5fc101b4af7cb54f
Steven Kofi Nnuro <48254031+stevennuro991@users.noreply.github.com>
2024-10-31 18:03:08 +0000

    Sign up integration

--- third_proj/lib/main.dart
+++ third_proj/lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:third_proj/helper/api_services.dart';
 import 'package:third_proj/screens/chats.dart';
-import 'package:third_proj/screens/logIn.dart';
+import 'package:third_proj/screens/logIn_screen.dart';
 import 'package:third_proj/screens/main_screen.dart';
 import 'package:third_proj/screens/onboarding1.dart';
 import 'package:third_proj/screens/onboarding2.dart';
 import 'package:third_proj/screens/onboarding3.dart';
 import 'package:third_proj/screens/orders.dart';
 import 'package:third_proj/screens/profile.dart';
-import 'package:third_proj/screens/signUp.dart';
+import 'package:third_proj/screens/signup_screen.dart';
 import 'package:third_proj/screens/splash.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (_) => ApiService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,25 +30,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/splash': (context) => Splash(),
+        '/onboarding1': (context) => Onboarding1(),
+        '/onboarding2': (context) => Onboarding2(),
+        '/onboarding3': (context) => Onboarding3(),
+        '/signUp': (context) => SignUp(),
+        '/logIn': (context) => LogIn(),
+        '/Order': (context) => Orders(),
+        '/Chats': (context) => Chats(),
+        '/Prifile': (context) => Profile(),
+        '/Mainscreen': (context) => MainScreen(),
 
-    initialRoute: '/',
-    routes: {
-      '/splash': (context) => Splash(),
-      '/onboarding1': (context) => Onboarding1(),
-      '/onboarding2': (context) => Onboarding2(),
-      '/onboarding3': (context) => Onboarding3(),
-      '/signUp': (context) => SignUp(),
-      '/logIn': (context) => LogIn() ,
-      '/Order': (context) => Orders(),
-      '/Chats': (context) => Chats(),
-      '/Prifile': (context) => Profile(),
-      '/Mainscreen': (context) => MainScreen(),
-
-      // Add more routes as needed
-    },
+        // Add more routes as needed
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -48,4 +55,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
