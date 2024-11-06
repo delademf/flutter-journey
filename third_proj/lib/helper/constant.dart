d4302527ed9c2919835f0a108cae0daf60883801
delademf <deladfiakeye@gmail.com>
2024-11-06 16:53:28 +0000

    new

--- third_proj/lib/helper/constant.dart
+++ third_proj/lib/helper/constant.dart
@@ -1,4 +1,4 @@
-const String baseUrl = "https://8989-196-61-37-18.ngrok-free.app";
+const String baseUrl = "https://3020-196-61-37-18.ngrok-free.app";
 
 
 Map<String, String> headers = {"Content-Type": "application/json"};
